4266bea602a98dbdf7e53aba63bcc61bd98728b2
pdulvp <pdulvp@laposte.net>
2025-01-31 17:15:14 +0100

    #125 Update to net8.0

--- plugins/Jellyfin.Plugin.QnapConfiguration/copy.bat
+++ plugins/Jellyfin.Plugin.QnapConfiguration/copy.bat
@@ -1,5 +1,3 @@
-mkdir "bin\server-1077\database\plugins\Jellyfin.Plugin.QnapConfiguration"
-mkdir "bin\server-1080\database\plugins\Jellyfin.Plugin.QnapConfiguration"
+mkdir "bin\server-10100\database\plugins\Jellyfin.Plugin.QnapConfiguration"
 
-copy "bin\Debug\net5.0\Jellyfin.Plugin.QnapConfiguration.*" "bin\server-1077\database\plugins\Jellyfin.Plugin.QnapConfiguration"
-copy "bin\Debug\net6.0\Jellyfin.Plugin.QnapConfiguration.*" "bin\server-1080\database\plugins\Jellyfin.Plugin.QnapConfiguration"
+copy "bin\Debug\net8.0\Jellyfin.Plugin.QnapConfiguration.*" "bin\server-10100\database\plugins\Jellyfin.Plugin.QnapConfiguration"

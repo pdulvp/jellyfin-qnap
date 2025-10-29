8b7348c9dde5f458865f8078c93aa91f91abcb7e
pdulvp <pdulvp@laposte.net>
2025-10-29 22:29:29 +0100

    Update to 10.11.1

--- jellyfin-info.sh
+++ jellyfin-info.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "JELLYFIN_VERSION=10.11.0" >> /.env
+echo "JELLYFIN_VERSION=10.11.1" >> /.env
 VERSION=$(/usr/lib/jellyfin-ffmpeg/ffmpeg -version | cut -d' ' -f3 | head -1 | cut -d'-' -f1 | head -1)
 echo "JELLYFIN_FFMPEG_VERSION=$VERSION" >> /.env
 env >> /.env
